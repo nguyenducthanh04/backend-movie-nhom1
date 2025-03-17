b25d3baa9c4e02c5023d9e3ced78d91c1a5ed0b4
unknown <dducthanh04@gmail.com>
2025-03-18 01:46:13 +0700

    update code

--- config/config.js
+++ config/config.js
@@ -1,30 +1,3 @@
-// require("dotenv").config();
-// module.exports = {
-//     development: {
-//         username: process.env.DB_USER,
-//         password: process.env.DB_PASS,
-//         database: process.env.DB_DATABASE,
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         dialect: process.env.DB_DRIVER,
-
-//         logging: true,
-//     },
-//     test: {
-//         username: process.env.DB_USER,
-//         password: process.env.DB_PASS,
-//         database: process.env.DB_DATABASE,
-//         host: process.env.DB_HOST,
-//         dialect: process.env.DB_DRIVER,
-//     },
-//     production: {
-//         username: process.env.DB_USER,
-//         password: process.env.DB_PASS,
-//         database: process.env.DB_DATABASE,
-//         host: process.env.DB_HOST,
-//         dialect: process.env.DB_DRIVER,
-//     },
-// };
 require("dotenv").config();
 module.exports = {
     development: {
