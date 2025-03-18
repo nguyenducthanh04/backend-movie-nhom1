f12afc396765ee0cca3b17dce90e8aa591ae8d68
unknown <dducthanh04@gmail.com>
2025-03-18 11:55:59 +0700

    ok

--- config/config.js
+++ config/config.js
@@ -1,53 +1,3 @@
-// require("dotenv").config();
-
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_DATABASE,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: process.env.DB_DRIVER,
-//     dialectOptions: {
-//       ssl: {
-//         require: true, // Bắt buộc với Supabase
-//         rejectUnauthorized: false // Bỏ qua lỗi chứng chỉ (dùng tạm thời)
-//       },
-//       family: 4 // Ép dùng IPv4 để tránh vấn đề IPv6
-//     },
-//     logging: true, // Bật log để debug
-//   },
-//   test: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_DATABASE,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: process.env.DB_DRIVER,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       },
-//       family: 4
-//     },
-//   },
-//   production: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_DATABASE,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: process.env.DB_DRIVER,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       },
-//       family: 4
-//     },
-//   },
-// };
 require("dotenv").config();
 
 module.exports = {
@@ -60,12 +10,11 @@ module.exports = {
     dialect: process.env.DB_DRIVER,
     dialectOptions: {
       ssl: {
-        require: true,
-        rejectUnauthorized: false
+        require: true, // Bắt buộc với Supabase
+        rejectUnauthorized: false // Bỏ qua lỗi chứng chỉ (dùng tạm thời)
       },
-      family: 4
     },
-    logging: true,
+    logging: true, // Bật log để debug
   },
   test: {
     username: process.env.DB_USER,
@@ -79,7 +28,6 @@ module.exports = {
         require: true,
         rejectUnauthorized: false
       },
-      family: 4
     },
   },
   production: {
@@ -94,7 +42,6 @@ module.exports = {
         require: true,
         rejectUnauthorized: false
       },
-      family: 4
     },
   },
 };
--- models/index.js
+++ models/index.js
@@ -1,20 +1,20 @@
-// 'use strict';
+'use strict';
 
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-// const process = require('process');
-// const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.js')[env];
-// const db = {};
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
 
-// // let sequelize;
-// // if (config.use_env_variable) {
-// //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// // } else {
-// //   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// // }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 // let sequelize;
 
 // if (config.use_env_variable) {
@@ -40,83 +40,6 @@
 //     }
 //   });
 // }
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (
-//       file.indexOf('.') !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === '.js' &&
-//       file.indexOf('.test.js') === -1
-//     );
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-const db = {};
-
-console.log('Environment:', env); // Log để kiểm tra môi trường
-console.log('Database Config:', config); // Log để kiểm tra config
-
-let sequelize;
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    ...config,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      },
-      family: 4 // Ép dùng IPv4
-    }
-  });
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    ...config,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      },
-      family: 4 // Ép dùng IPv4
-    }
-  });
-}
-
-// Kiểm tra kết nối khi khởi động
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
-
 fs
   .readdirSync(__dirname)
   .filter(file => {
